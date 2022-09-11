4d59ca212a32a7002ac3c41079a13578eef45b1e
prakashanki526 <111894989+prakashanki526@users.noreply.github.com>
2022-09-11 11:36:14 +0530

    Time: 23 ms (54.55%), Space: 13.4 MB (10.85%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int>r,c;
+        int n=matrix.size(),m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    r.insert(i);
+                    c.insert(j);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(r.find(i)!=r.end() || c.find(j)!=c.end())
+                    matrix[i][j]=0;
+            }
+        }
+    }
+};
