2a606b2792fd6b27a483da1b8c1bf18f68e4a4aa
prakashanki526 <111894989+prakashanki526@users.noreply.github.com>
2022-10-04 12:39:23 +0530

    Time: 70 ms (31.67%), Space: 32.8 MB (41.44%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* findpos(TreeNode* root){
+        if(root->right == NULL)
+            return root;
+        while(root->right!=NULL)
+            root = root->right;
+        return root;
+    }
+    TreeNode* helper(TreeNode* root){
+        if(!root->left)
+            return root->right;
+        if(!root->right)
+            return root->left;
+        TreeNode* rightchild = root->right;
+        TreeNode* postoinsert = findpos(root->left);
+        postoinsert->right = rightchild;
+        return root->left;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL)
+            return root;
+        if(root->val == key){
+            root = helper(root);
+            return root;
+        }
+        TreeNode* res = root;
+        while(root!=NULL){
+            if(root->left && root->left->val == key){
+                root->left = helper(root->left);
+                break;
+            }
+            if(root->right && root->right->val ==key){
+                root->right = helper(root->right);
+                break;
+            }
+            if(root->val<key)
+                root = root->right;
+            else
+                root = root->left;
+        }
+        return res;
+    }
+};
