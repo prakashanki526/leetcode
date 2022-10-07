b1d6843ee2da205f1b3fadc7b028fe2c7dbf9547
prakashanki526 <111894989+prakashanki526@users.noreply.github.com>
2022-10-07 19:55:34 +0530

    Time: 311 ms (18.25%), Space: 26.4 MB (37.06%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -8,13 +8,13 @@ public:
     
     int book(int start, int end) {
         mp[start]++;
-    mp[end]--;
-    int count = 0;
-    for(auto it = mp.begin();it != mp.end();it++){
-        count += it->second;
-        maxCount = max(count,maxCount);
-    }
-    return maxCount;
+        mp[end]--;
+        int count = 0;
+        for(auto it:mp){
+            count += it.second;
+            maxCount = max(count,maxCount);
+        }
+        return maxCount;
     }
 };
 
