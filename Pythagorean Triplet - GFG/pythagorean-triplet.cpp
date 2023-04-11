8ceebeeccf0872471cb8801ec471e0a8fbe070ab
prakashanki526 <111894989+prakashanki526@users.noreply.github.com>
2023-04-11 22:38:15 +0530

    Added solution - LeetHub

---
+++ Pythagorean Triplet - GFG/pythagorean-triplet.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// Function to check if the
+	// Pythagorean triplet exists or not
+	bool checkTriplet(int arr[], int n) {
+	    // code here
+	    unordered_set<int>s;
+	    for(int i=0;i<n;i++)
+	        s.insert(arr[i] * arr[i]);
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            if(s.find(arr[i]*arr[i] + arr[j]*arr[j])!=s.end())
+	                return true;
+	        }
+	    }
+	    return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.checkTriplet(arr, n);
+        if (ans) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
